631f77b00d3cb07472f2af19fb4541f2e1fd622e
mroyon2000 <mroyon2000@gmail.com>
2024-01-08 01:43:07 +0300

    test

--- Digi Com/AppForms/Cryptography.cs
+++ Digi Com/AppForms/Cryptography.cs
@@ -50,7 +50,7 @@ namespace Digi_Com.AppForms
             string fileExtension = Path.GetExtension(fileOriginal.FullName);
             long fileSize = fileOriginal.Length;
 
-            string outputPath = Path.Combine(directoryPath, fileNameWithoutExtension + ".aes");
+            string outputPath = Path.Combine(directoryPath, fileNameWithoutExtension + fileExtension + ".aes");
             File.WriteAllText(outputPath, encryptedString);
 
         }
@@ -63,58 +63,15 @@ namespace Digi_Com.AppForms
         /// <param name="password"></param>
         public void FileDecrypt(string inputFile, string outputFile, string password)
         {
-            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
-            byte[] salt = new byte[32];
+            FileInfo fileOriginal = new FileInfo(inputFile);
+            string encryptedString = File.ReadAllText(fileOriginal.FullName);
 
-            FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
-            fsCrypt.Read(salt, 0, salt.Length);
+            string directoryPath = fileOriginal.Directory.FullName;
+            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileOriginal.FullName);
+            string fileExtension = Path.GetExtension(fileOriginal.FullName);
+            long fileSize = fileOriginal.Length;
 
-            RijndaelManaged AES = new RijndaelManaged();
-            AES.KeySize = 256;
-            AES.BlockSize = 128;
-            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
-            AES.Key = key.GetBytes(AES.KeySize / 8);
-            AES.IV = key.GetBytes(AES.BlockSize / 8);
-            AES.Padding = PaddingMode.PKCS7;
-            AES.Mode = CipherMode.CFB;
 
-            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
-
-            FileStream fsOut = new FileStream(outputFile, FileMode.Create);
-
-            int read;
-            byte[] buffer = new byte[1048576];
-
-            try
-            {
-                while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
-                {
-                    Application.DoEvents();
-                    fsOut.Write(buffer, 0, read);
-                }
-            }
-            catch (CryptographicException ex_CryptographicException)
-            {
-                Console.WriteLine("CryptographicException error: " + ex_CryptographicException.Message);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error: " + ex.Message);
-            }
-
-            try
-            {
-                cs.Close();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error by closing CryptoStream: " + ex.Message);
-            }
-            finally
-            {
-                fsOut.Close();
-                fsCrypt.Close();
-            }
         }
     }
 
